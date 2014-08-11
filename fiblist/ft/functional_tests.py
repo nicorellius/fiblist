feca88e9ef5c51385b38c7baa51efd4cfe6a9a85
nicorellius <nicorellius@gmail.com>
2014-08-11 15:53:10 -0700

    added unit test class for functional test file

--- fiblist/ft/functional_tests.py
+++ fiblist/ft/functional_tests.py
@@ -7,9 +7,45 @@ desription  :   functional tests for aersol project
 """
 
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-#for stock Django installation. this will probably change once app is built out
-assert 'Django' in browser.title
+# Testing basic app functionality.
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+        
+    def tearDown(self):
+        
+        self.browser.quit()
+        
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        
+        # First, make sure browser opened with correct page.
+        self.browser.get('http://localhost:8000')
+        
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+        
+        # User is invited to enter a to-do item in application.
+    
+        # User types in "some list item" into text box.
+        
+        # When user hits enter, the page updates and the list item is saved.
+        
+        # The field is still present, so uer can add another list item, if desired.
+        
+        # User is concerned whether this site will actually remember the items entered.
+        # The site generates a URL for each list item, so this is good.
+        
+        # User clicks the URL and notices the list item is there.
+        
+        # End user story
+        
+        
+if __name__ == '__main__':
+    
+    unittest.main(warnings='ignore')
