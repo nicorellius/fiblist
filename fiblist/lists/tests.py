879eb781c860203351a45dfd05e1a2325f4c3d83
nicorellius <nicorellius@gmail.com>
2014-08-12 10:56:12 -0700

    added home page test to check http response for well formed html code

--- fiblist/lists/tests.py
+++ fiblist/lists/tests.py
@@ -8,6 +8,7 @@ desription  :   tests for the lists application
 
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 
 from .views import home_page
 
@@ -24,4 +25,13 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         
         found = resolve('/')
-        self.assertEqual(found.func, home_page)    
+        self.assertEqual(found.func, home_page)
+        
+    def test_home_page_returns_correct_html(self):
+        
+        request = HttpRequest()
+        response = home_page(request)
+        
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do Lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
