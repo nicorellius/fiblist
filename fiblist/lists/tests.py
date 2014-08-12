fc55bc7df043dcdca263231ae3870eba519d10ec
nicorellius <nicorellius@gmail.com>
2014-08-12 10:42:34 -0700

    added root resolver test to check new home page function view

--- fiblist/lists/tests.py
+++ fiblist/lists/tests.py
@@ -6,12 +6,22 @@ classes     :
 desription  :   tests for the lists application
 """
 
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+
+from .views import home_page
 
 
 class SmokeTest(TestCase):
     
-    def test_bad_maths(self):
+    def test_bad_math(self):
         
-        self.assertEqual(1 + 1, 3)
+        self.assertEqual(1 + 1, 2)
+
+
+class HomePageTest(TestCase):
     
+    def test_root_url_resolves_to_home_page_view(self):
+        
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)    
