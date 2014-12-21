393c93cd783ce763e09851d1a48c9f4dbf6cd101
nicorellius <nicorellius@gmail.com>
2014-12-21 14:47:37 -0800

    sqlite, and tests, and requirements updates

--- fiblist/fiblist/settings.py
+++ fiblist/fiblist/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'debug_toolbar',
 )
 
 MIDDLEWARE_CLASSES = (
--- fiblist/functional_tests/tests.py
+++ fiblist/functional_tests/tests.py
@@ -23,21 +23,18 @@ class NewVisitorTest(LiveServerTestCase):
         
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(1)
-        
-        
+
     def tearDown(self):
         
         self.browser.quit()
- 
-        
+
     def check_for_row_in_list_table(self, row_text):
         
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         
         self.assertIn(row_text, [row.text for row in rows])
-    
-        
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         
         # First, make sure browser opened with correct page.
