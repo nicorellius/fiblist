2bc388c25f85cd7c4a69007ecbb1f0d2d751c840
Nick Vincent-Maloney <nicorellius@gmail.com>
2016-03-29 21:59:34 -0700

    minor tweaks to wsgi, settings and urls for static, other rearrangement.

--- source/fiblist/settings.py
+++ source/fiblist/settings.py
@@ -84,4 +84,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'lists/static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
--- source/fiblist/wsgi.py
+++ source/fiblist/wsgi.py
@@ -12,7 +12,7 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append('/home/nick/dev/django/projects/fiblist/fiblist')
+sys.path.append('/home/nick/dev/django/projects/fiblist/source')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fiblist.settings")
 
