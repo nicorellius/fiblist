3cc7fd138bec21cd52063fdcf609d2efe9479846
Nick Vincent-Maloney <nicorellius@gmail.com>
2016-04-01 16:54:18 -0700

    set up basics for fabfile.

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -7,4 +7,13 @@ REPO_URL = 'https://github.com/nicorellius/fiblist.git'
 
 
 def deploy():
-    pass
+
+    site_folder = '/home/{0}/sites/{1}'.format(env.user, env.host)
+    source_folder = '{0}/{1}'.format(site_folder, '/source')
+
+    _create_directory_structure_if_necessary(site_folder)
+    _get_latest_source(source_folder)
+    _update_ssettings(source_folder, env.host)
+    _update_virtenv(source_folder)
+    _update_static_diles(source_folder)
+    _update_database(source_folder)
