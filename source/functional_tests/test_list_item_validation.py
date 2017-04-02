7b6903198632d3842c987a761b49afcfef0efafb
Nick Vincent-Maloney <nicorellius@gmail.com>
2017-04-02 14:04:08 -0700

    Cleaned up code, pep8, remove white space, etc.

--- source/functional_tests/test_list_item_validation.py
+++ source/functional_tests/test_list_item_validation.py
@@ -1,10 +1,7 @@
-"""
-file        :   test_list_item_validation.py
-date        :   2014-0811
-module      :   functional_tests
-classes     :
-description :   functional tests for fiblist project
-"""
+# file: test_list_item_validation.py
+# date: 2014-0811
+# description: functional tests for fiblist project
+
 
 from .base import FunctionalTest
 
@@ -15,17 +12,19 @@ class ItemValidationTest(FunctionalTest):
 
     def test_cannot_add_empty_list_items(self):
 
-        # User goes to the home page and enters empty item. Hits enter to submit.
+        # User goes to home page and enters empty item. Hits enter to submit.
         self.browser.get(self.server_url)
         self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
-        # Home page refreshes, and user gets an error message saying empty lists
-        # are invalid.
+        # Home page refreshes, and user gets an error message
+        # saying empty lists are invalid.
         error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You cannot submit an empty list item.")
 
         # User then enters a valid item and hists enter. This works.
-        self.browser.find_element_by_id('id_new_item').send_keys('Flash light\n')
+        self.browser.find_element_by_id('id_new_item').send_keys(
+            'Flash light\n'
+        )
         self.check_for_row_in_list_table('1: Flash light')
 
         # User tries again to enter empty item, error message.
@@ -37,7 +36,9 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, "You cannot submit an empty list item.")
 
         # User can correct this error by entering valid list item.
-        self.browser.find_element_by_id('id_new_item').send_keys('Batteries\n')
+        self.browser.find_element_by_id('id_new_item').send_keys(
+            'Batteries\n'
+        )
         self.check_for_row_in_list_table('1: Flash light')
         self.check_for_row_in_list_table('2: Batteries')
 
